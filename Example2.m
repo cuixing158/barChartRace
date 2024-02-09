d807028225d0337ed033dc0aa91e996631876a43
cuixingxing150 <cuixingxing150@gmail.com>
2024-02-09 05:35:05 +0000

    chore: add sample data

--- Example2.m
+++ Example2.m
@@ -1,18 +1,17 @@
 %% 全国房价
-filename = './dataSets/主要城市年度数据 -住宅平均销售价格.csv';
-inData = readmatrix(filename,'Range','B5:T39');
-labelnames = readcell(filename,'Range','A5:A39');
-timesStr = readcell(filename,'Range','B4:T4');
-inData = fillmissing(inData,'linear',2,'EndValues','nearest');
+filename = './dataSets/2003-2022国内主要城市住宅商品房平均销售价格.csv';
+inData = readmatrix(filename,'Range','B5:U40');
+labelnames = readcell(filename,'Range','A5:A40');
+timesStr = string(2003:2022);
+inData = fillmissing(inData,'constant',0.1);
 
 % 整理成合适的格式
 inData = fliplr(inData);
-timesStr = fliplr(timesStr);
 labels = categorical(labelnames);
 h = barChartRace2(inData,...
     "TopK",10,...
     "Categories",labels,...
     "TimeLabels",timesStr,...
     "FramesPerDataTick",100,...
-    "TitleLabel",'主要城市年度数据 -2001-2019年住宅平均销售价格（元/平米）',...
+    "TitleLabel",'2003-2022国内主要城市住宅商品房平均销售价格（元/平米）',...
     "GenerateGIF",false);
